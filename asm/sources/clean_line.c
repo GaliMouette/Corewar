6ca2ddd369a3c67bcd2318007aae1a1dcb3a0e28
Alexis Carré <alexis.carre@epitech.eu>
2020-04-27 19:33:49 +0000

    Added function to remove comas while cleaning line

--- asm/sources/clean_line.c
+++ asm/sources/clean_line.c
@@ -10,6 +10,8 @@
 
 static void remove_tabulations(char *line);
 
+static void remove_comas(char *line);
+
 static void remove_spaces(char *line);
 
 int clean_line(char *line)
@@ -25,6 +27,7 @@ int clean_line(char *line)
         my_strstr(line, "#")[0] = '\0';
     }
     remove_tabulations(line);
+    remove_comas(line);
     remove_spaces(line);
     if (!line[0]) {
         return 1;
@@ -44,6 +47,24 @@ static void remove_tabulations(char *line)
             quotes = 0;
         }
         if (!quotes && '\t' == line[i]) {
+            line[i] = ' ';
+        }
+        i++;
+    }
+}
+
+static void remove_comas(char *line)
+{
+    int i = 0;
+    int quotes = 0;
+
+    while (line[i]) {
+        if ('\"' == line[i] && !quotes) {
+            quotes = 1;
+        } else if ('\"' == line[i] && quotes) {
+            quotes = 0;
+        }
+        if (!quotes && ',' == line[i]) {
             line[i] = ' ';
         }
         i++;
