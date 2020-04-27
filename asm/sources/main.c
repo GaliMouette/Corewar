f30407d7148116de7cec901501166861e4bb98e0
Alexis Carré <alexis.carre@epitech.eu>
2020-04-27 15:13:29 +0000

    Added error gestion on argument number

--- asm/sources/main.c
+++ asm/sources/main.c
@@ -13,6 +13,15 @@ int main(int argc, char const *argv[])
 {
     if (2 == argc && !my_strcmp("-h", argv[1])) {
         write(1, USAGE, 148);
+        return 0;
+    }
+    if (2 < argc) {
+        write(2, "Too many arguments.\n", 20);
+        return 84;
+    }
+    if (1 == argc) {
+        write(2, "Not enough arguments.\n", 22);
+        return 84;
     }
     return 0;
 }
