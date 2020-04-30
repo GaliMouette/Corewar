f3f99109e49cbba865bf56587efcdd2dce1c11a6
Lia l'IA <lia1>
2020-04-30 19:11:43 +0200

    Added test for function my_strcmp

---
+++ tests/test_my_strcmp.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_corewar_2019
+** File description:
+** test_my_strcmp.c
+*/
+
+#include <criterion/criterion.h>
+
+int my_strcmp(char const *str1, char const *str2);
+
+Test(my_strcmp, same_str)
+{
+    char *str1 = "Hello";
+    char *str2 = "Hello";
+
+    cr_assert_eq(my_strcmp(str1, str2), 0);
+}
+
+Test(my_strcmp, str1_big)
+{
+    char *str1 = "HelloT";
+    char *str2 = "Hello";
+
+    cr_assert_eq(my_strcmp(str1, str2), 84);
+}
+
+Test(my_strcmp, str2_big)
+{
+    char *str1 = "Hello";
+    char *str2 = "HelloT";
+
+    cr_assert_eq(my_strcmp(str1, str2), -84);
+}
