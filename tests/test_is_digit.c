f72e5d821f5be1dad07dcf7da408d9b2ccd0b0ad
Lia l'IA <lia1>
2020-04-30 19:08:01 +0200

    Added test for function is_digit

---
+++ tests/test_is_digit.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_corewar_2019
+** File description:
+** test_is_digit.c
+*/
+
+#include <criterion/criterion.h>
+
+int is_digit(char const *str);
+
+Test(is_digit, full_Digit)
+{
+    char *str = "117443";
+
+    cr_assert_eq(is_digit(str), 1);
+}
+
+Test(is_digit, no_Str)
+{
+    char *str = NULL;
+
+    cr_assert_eq(is_digit(str), 0);
+}
+
+Test(is_digit, letter_In_Str)
+{
+    char *str = "08u83";
+
+    cr_assert_eq(is_digit(str), 0);
+}
+
+Test(is_digit, negative_Digit)
+{
+    char *str = "-117443";
+
+    cr_assert_eq(is_digit(str), 1);
+}
