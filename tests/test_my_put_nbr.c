3f8ebb1bf5cca5d4379056f18cdd6d6b69498482
Lia l'IA <lia1>
2020-04-30 19:11:25 +0200

    Added test for function my_put_nbr

---
+++ tests/test_my_put_nbr.c
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_corewar_2019
+** File description:
+** test_my_put_nbr.c
+*/
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+void my_put_nbr(int number);
+
+void redirect_all_stdout(void)
+{
+    cr_redirect_stdout();
+}
+
+Test(my_put_nbr, simple_Number, .init=redirect_all_stdout)
+{
+    my_put_nbr(42);
+
+    cr_assert_stdout_eq_str("42");
+}
+
+Test(my_put_nbr, negative_Number, .init=redirect_all_stdout)
+{
+    my_put_nbr(-42);
+
+    cr_assert_stdout_eq_str("-42");
+}
