baca8c12185b2d76e861ab5db726ac03ada88033
Guichard Lucas <lucas.guichard@epitech.eu>
2020-05-01 18:37:36 +0200

    Sent value directly in function parameters for strcspn unit tests

--- tests/utils/test_my_strcspn.c
+++ tests/utils/test_my_strcspn.c
@@ -11,16 +11,10 @@ int my_strcspn(char const *str, char const *chars);
 
 Test(my_strcspn, char_Commun)
 {
-    char *str1 = "live:";
-    char *str2 = ":";
-
-    cr_assert_eq(my_strcspn(str1, str2), 4);
+    cr_assert_eq(my_strcspn("live:", ":"), 4);
 }
 
 Test(my_strcspn, no_Char_Commun)
 {
-    char *str1 = "Hello";
-    char *str2 = "skirt";
-
-    cr_assert_eq(my_strcspn(str1, str2), 5);
+    cr_assert_eq(my_strcspn("Hello", "skirt"), 5);
 }
