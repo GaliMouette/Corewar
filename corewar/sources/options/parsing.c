ae29ece422d5fdb69abe21566f650676915f96fb
Alexis Carré <alexis.carre@epitech.eu>
2020-05-04 18:02:45 +0000

    Added option parsing

--- corewar/sources/options/parsing.c
+++ corewar/sources/options/parsing.c
@@ -10,13 +10,37 @@
 int parse_args(char const *argv[], init_t *init)
 {
     int i = 1;
+    int index = 0;
 
     while (argv[i]) {
         if (!my_strcmp(argv[i], "-dump")) {
-            if (set_dump(argv[i], init)) {
+            if (set_dump(argv[i + 1], init)) {
                 return 1;
             }
+            i += 2;
+            continue;
+        }
+        if (set_prog_args(argv, &i, init, &index)) {
+            return 1;
         }
         i++;
     }
+    return 0;
+}
+
+static int set_prog_args(char const *argv[], int *i, init_t *init, int *index)
+{
+    if (!my_strcmp(argv[*i], "-n")) {
+        if (set_prog_nbr(argv[*i + 1], i, init, *index)) {
+            return 1;
+        }
+    } else if (!my_strcmp(argv[*i], "-a")) {
+        if (set_load_address(argv[*i + 1], i, init, *index)) {
+            return 1;
+        }
+    } else {
+        init->champs[*index].path = argv[*i];
+        (*index)++;
+    }
+    return 0;
 }
--- includes/corewar/options/parsing.h
+++ includes/corewar/options/parsing.h
@@ -10,5 +10,14 @@
 
 #include "typedefs/init_t.h"
 #include "utils.h"
+#include <unistd.h>
+
+int parse_args(char const *argv[], init_t *init);
+static int set_prog_args(char const *argv[], int *i, init_t *init, int *index);
+
+//  Using
+int set_dump(char const *arg, init_t *init);
+int set_prog_nbr(char const *arg, int *i, init_t *init, int champion);
+int set_load_address(char const *arg, int *i, init_t *init, int champion);
 
 #endif /* !PARSING_H_ */
