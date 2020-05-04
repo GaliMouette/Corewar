24382afe4149d67725db4b1757f56169ec295159
Alexis Carré <alexis.carre@epitech.eu>
2020-05-04 17:58:10 +0000

    Removed comment in my_strtol

--- utils/my_strtol.c
+++ utils/my_strtol.c
@@ -23,31 +23,3 @@ long my_strtol(const char *str, int base)
     }
     return result;
 }
-
-/*
-int my_puissance(int nb, int puissance)
-{
-    int result = 1;
-
-    for (int i = 0; i != puissance; i++)
-        result *= nb;
-    return result;
-}
-
-long my_base_to_deci(char *str, int base)
-{
-    long result = 0;
-    int rank = 0;
-
-    for (int i = my_strlen(str) - 1; str[i]; i--, rank++) {
-        if ('A' <= str[i] && str[i] <= 'F') {
-            result += (str[i] - ('A' - 10)) * my_puissance(base, rank);
-        } else if ('a' <= str[i] && str[i] <= 'f') {
-            result += (str[i] - ('a' - 10)) * my_puissance(base, rank);
-        } else {
-            result += (str[i] - '0') * my_puissance(base, rank);
-        }
-    }
-    return result;
-}
-*/
