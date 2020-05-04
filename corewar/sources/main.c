2b3e9018e75e81d9da15da71736f77eb39c1e8f1
Alexis Carré <alexis.carre@epitech.eu>
2020-05-04 18:02:59 +0000

    Updated main

--- corewar/sources/main.c
+++ corewar/sources/main.c
@@ -9,11 +9,13 @@
 
 int main(int argc, char const *argv[])
 {
-    init_t init = {-1, {0}};
+    init_t init = {-1, {{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}}};
 
     if (1 == argc || (2 == argc && !my_strcmp(argv[1], "-h"))) {
-        write(1, USAGE, 584);
+        write(1, USAGE, 580);
         return 0;
     }
+    if (parse_args(argv, &init))
+        return 84;
     return 0;
 }
