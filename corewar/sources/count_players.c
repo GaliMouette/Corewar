d374468c91ec9db37963a2a1497d7b82156c9ab0
Guichard Lucas <lucas.guichard@epitech.eu>
2020-05-09 21:52:30 +0200

    Added function to count nb_players and his header

---
+++ corewar/sources/count_players.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2020
+** Visual Studio Live Share (Workspace)
+** File description:
+** count_players
+*/
+
+#include "corewar/count_players.h"
+
+int count_players(init_t *init)
+{
+    int nb_players = 0;
+
+    for (int i = 0; i < 4; i++) {
+        if (init->champs[nb_players].path) {
+            nb_players++;
+        }
+    }
+    return nb_players;
+}
