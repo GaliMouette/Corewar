bd0ffcf73d9e82418e9c4f3d38a2fdac45e33860
Guichard Lucas <lucas.guichard@epitech.eu>
2020-05-12 09:43:45 +0200

    Added non-funtional <fork> and <lfork> mnemoniques

---
+++ corewar/sources/execution/instructions/fork.c
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2020
+** Visual Studio Live Share (Workspace)
+** File description:
+** fork
+*/
+#include "corewar/execution/instructions/instructions.h"
+#include <stdlib.h>
+
+int fork(arena_t *arena, int i)
+{
+    int direct = arena->execs[i]->loaded_op.args[0];
+    int nb_execs = 0;
+
+    while (arena->execs[nb_execs]) {
+        nb_execs++;
+    }
+    arena->execs = realloc(arena->execs, sizeof(arena->execs) + 1); //TODO null
+    if (!arena->execs) {
+        return 1;
+    }
+    arena->nb_players += 1;
+    arena->execs[arena->nb_players] = NULL;
+    arena->execs[arena->nb_players - 1] = malloc(sizeof(*arena->execs[i]));
+    if (!arena->execs[arena->nb_players - 1]) {
+        return 1;
+    }
+    arena->execs[arena->nb_players - 1]->carry = arena->execs[i]->carry;
+    arena->execs[arena->nb_players - 1]->pc = (arena->execs[i]->pc + direct) % IDX_MOD;
+    my_memcpy(arena->execs[arena->nb_players - 1]->registry, arena->execs[i]->registry, 16);
+    arena->execs[arena->nb_players - 1]->registry[0] = arena->nb_players;
+    arena->execs[arena->nb_players - 1]->loaded_op.is_op_loaded = arena->execs[i]->loaded_op.is_op_loaded;
+    arena->execs[arena->nb_players - 1]->loaded_op.opcode = arena->execs[i]->loaded_op.opcode;
+    my_memcpy(arena->execs[arena->nb_players - 1]->loaded_op.args_type, arena->execs[i]->loaded_op.args_type, 4);
+    my_memcpy(arena->execs[arena->nb_players - 1]->loaded_op.args_size,arena->execs[i]->loaded_op.args_size, 4);
+    my_memcpy(arena->execs[arena->nb_players - 1]->loaded_op.args, arena->execs[i]->loaded_op.args, 4);
+    arena->execs[arena->nb_players - 1]->loaded_op.pc_offset = arena->execs[i]->loaded_op.pc_offset;
+    arena->execs[arena->nb_players - 1]->loaded_op.wait_cycle = arena->execs[i]->loaded_op.wait_cycle;
+}
+
+//TODO nb_execs
---
+++ corewar/sources/execution/instructions/lfork.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** Visual Studio Live Share (Workspace)
+** File description:
+** lfork
+*/
+
+#include "corewar/execution/instructions/instructions.h"
+
+int lfork(arena_t *arena, int i)
+{
+
+}
