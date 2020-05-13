408c2bbf5d96b4a67765ad596893aa26876aa09b
Alexis Carré <alexis.carre@epitech.eu>
2020-05-13 09:37:29 +0000

    Removed comments in fork

--- corewar/sources/execution/instructions/fork.c
+++ corewar/sources/execution/instructions/fork.c
@@ -7,7 +7,6 @@
 
 #include "corewar/execution/instructions/instructions.h"
 
-
 int fork_instruction(arena_t *arena, int i)
 {
     int offset = arena->execs[i]->loaded_op.args[0] % IDX_MOD;
@@ -29,24 +28,3 @@ int fork_instruction(arena_t *arena, int i)
     arena->execs[nb_exec]->pc = pc;
     return 0;
 }
-
-// void dup_program(arena_t *arena, int i, int nb_exec)
-// {
-    // arena->execs[nb_exec]->carry = arena->execs[i]->carry;
-    // my_memcpy(arena->execs[nb_exec]->registry, arena->execs[i]->registry, 16);
-    // arena->execs[nb_exec]->registry[0] = arena->nb_exec;
-    // arena->execs[nb_exec]->loaded_op.is_op_loaded =
-    // arena->execs[i]->loaded_op.is_op_loaded;
-    // arena->execs[nb_exec]->loaded_op.opcode =
-    // arena->execs[i]->loaded_op.opcode;
-    // my_memcpy(arena->execs[nb_exec]->loaded_op.args_type,
-    // arena->execs[i]->loaded_op.args_type, 4);
-    // my_memcpy(arena->execs[nb_exec]->loaded_op.args_size,
-    // arena->execs[i]->loaded_op.args_size, 4);
-    // my_memcpy(arena->execs[nb_exec]->loaded_op.args,
-    // arena->execs[i]->loaded_op.args, 4);
-    // arena->execs[nb_exec]->loaded_op.pc_offset =
-    // arena->execs[i]->loaded_op.pc_offset;
-    // arena->execs[nb_exec]->loaded_op.wait_cycle =
-    // arena->execs[i]->loaded_op.wait_cycle;
-// }
