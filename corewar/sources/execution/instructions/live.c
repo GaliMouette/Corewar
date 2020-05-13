2ecf0093652bc14b711a1912caf2fe8848b7046a
Alexis Carré <alexis.carre@epitech.eu>
2020-05-13 09:09:04 +0000

    Reworked live instruction

--- corewar/sources/execution/instructions/live.c
+++ corewar/sources/execution/instructions/live.c
@@ -7,13 +7,23 @@
 
 #include "corewar/execution/instructions/instructions.h"
 
+// TODO : live_delta décrémenter
+
 int live_instruction(arena_t *arena, int i)
 {
-    arena->players[i].status = ALIVE;
+    loaded_op_t loaded_op = arena->execs[i]->loaded_op;
+    int index = loaded_op.args[0] - 1;
+    char *name;
+
+    if (index < 0 || arena->nb_players <= index) {
+        return 0;
+    }
+    name = arena->players[index].name;
+    arena->players[index].status = ALIVE;
     write(1, "The player ", 12);
-    my_put_nbr(arena->players[i].number);
+    my_put_nbr(arena->players[index].number);
     write(1, " (", 2);
-    write(1, arena->players[i].name, (size_t)my_strlen(arena->players[i].name));
+    write(1, name, (size_t) my_strlen(name));
     write(1, ") is alive.\n", 12);
     return 0;
 }
