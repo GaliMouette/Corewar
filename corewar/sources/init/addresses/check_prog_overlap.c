66c187a05b7f3540778ff06bd690f533d8ddc5d2
Alexis Carré <alexis.carre@epitech.eu>
2020-05-16 18:27:06 +0000

    Repaired check_overlap

--- corewar/sources/init/addresses/check_prog_overlap.c
+++ corewar/sources/init/addresses/check_prog_overlap.c
@@ -16,13 +16,13 @@ int check_prog_overlap(init_t *init)
             continue;
         }
         s1 = init->champs[i].load_address % MEM_SIZE;
-        e1 = s1 + init->champs[i].header.prog_size % MEM_SIZE;
+        e1 = (s1 + init->champs[i].header.prog_size - 1) % MEM_SIZE;
         for (int j = i + 1; j < 4; j++) {
             if (!(init->champs[j].path)) {
                 continue;
             }
             s2 = init->champs[j].load_address % MEM_SIZE;
-            e2 = s2 + init->champs[j].header.prog_size % MEM_SIZE;
+            e2 = (s2 + init->champs[j].header.prog_size - 1) % MEM_SIZE;
             if ((s1 <= s2 && s2 <= e1) || (s2 <= e1 && e1 <= e2)) {
                 write(2, "Program overlap detected.\n", 26);
                 return 1;
